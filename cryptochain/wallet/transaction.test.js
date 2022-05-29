78a4c5044eaf1c91a4f5cef709cd0343e6a8f9d6
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2022-05-29 19:25:42 +0200

    rewardTransaction()

--- cryptochain/wallet/transaction.test.js
+++ cryptochain/wallet/transaction.test.js
@@ -2,6 +2,7 @@ const { intFromLE } = require('elliptic/lib/elliptic/utils');
 const Wallet = require('.');
 const Transaction = require('./transaction');
 const { verifySignature } = require('../util');
+const { REWARD_INPUT, MINING_REWARD } = require('../config');
 
 describe('Transaction', () => {
     let transaction,senderWallet, recipient, amount;
@@ -163,4 +164,21 @@ describe('Transaction', () => {
           });
         });
       });
+
+    describe('rewardTransaction()', () => {
+        let rewardTransaction, minerWallet;
+
+        beforeEach(() => {
+            minerWallet = new Wallet();
+            rewardTransaction = Transaction.rewardTransaction({ minerWallet });
+        });
+
+        it('creates a transaction with the reward input', () => {
+            expect(rewardTransaction.input).toEqual(REWARD_INPUT);
+        });
+
+        it('creates one transaction for the miner with the `MINING_REWARD`', () => {
+            expect(rewardTransaction.outputMap[minerWallet.publicKey]).toEqual(MINING_REWARD);
+        });
+    });
 });
